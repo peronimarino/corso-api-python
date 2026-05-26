4fed74811fb5d4b6589cb330716e622924b7056a
peronimarino <peronimarino@gmail.com>
2026-05-26 14:39:52 +0000

    aggiunto_api_data

---
+++ registro.py
@@ -0,0 +1,17 @@
+voti=[4,6,9,8]
+
+voti.append(9)
+
+voti.insert(2,"7-")
+
+
+
+for voto in voti:
+    print(voto)
+
+x=voti.pop(5)
+print (f"voto rimosso: {x}")
+
+for voto in voti:
+    print(voto)
+
